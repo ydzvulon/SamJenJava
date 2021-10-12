2541d529de622eeb0c03b377af69d8fa2e3b83e5
Yair Dar <zvulon+fork.yairv.dar@gmail.com>
2021-10-13 00:26:26 +0300

    fix package naming

--- JenJavaSam/src/main/java/com/jenjavasam/App.java
+++ JenJavaSam/src/main/java/com/jenjavasam/App.java
@@ -1,4 +1,4 @@
-package com.samjenjava;
+package com.jenjavasam;
 
 /**
  * Hello world!
--- JenJavaSam/src/test/java/com/jenjavasamtest/AppTest.java
+++ JenJavaSam/src/test/java/com/jenjavasamtest/AppTest.java
@@ -1,4 +1,4 @@
-package com.samjenjava;
+package com.jenjavasamtest;
 
 import org.junit.jupiter.api.Test;
 
